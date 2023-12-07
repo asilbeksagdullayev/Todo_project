6b195a71b5bdc38cfb738a5bf99d88c858277cf3
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2023-12-07 11:10:19 +0500

    whats up

---
+++ web/src/index.ts
@@ -0,0 +1,15 @@
+const baseURL = 'http://localhost:4000/api';
+
+async function getTodos() {
+	const res = await fetch(`${baseURL}/todos`);
+	const data = await res.json();
+
+	return data;
+}
+
+async function init() {
+	const todos = await getTodos();
+	console.log('todos = ', todos);
+}
+
+window.addEventListener('load', init);
